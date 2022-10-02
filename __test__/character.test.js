72c339321e9d8132a4753e247f9a51be5dabf76c
pphila <efl2ice89@gmail.com>
2022-10-02 11:20:04 -0700

    add pass to second test
    Co-authored-by: David Marchuk
    <davormarchuk@gmail.com>

--- __test__/character.test.js
+++ __test__/character.test.js
@@ -2,15 +2,24 @@ import { TestScheduler } from "jest";
 import Character from "./../src/character.js";
 
 describe("Character", () => {
-  
   test('should create a new character object with level, attributes, car type', () => {
     const charName = "Dave";
-    const charClass = "mage";
-    const charCar = "muscle";
+    const charCar = "electric";
     
-    const newCharacter = new Character(charName, charClass, charCar);
+    const newCharacter = new Character(charName,charCar);
     expect(newCharacter.charName).toEqual(charName);
-    expect(newCharacter.charClass).toEqual(charClass);
     expect(newCharacter.charCar).toEqual(charCar);
   });
+
+  test('should set attributes for horsepower,torque,nos based on car class and type', ()=> {
+    const charName = "Dave";
+    const charCar = "electric";
+    
+    const newCharacter = new Character(charName, charCar);
+    newCharacter.setAttributes();
+    expect(newCharacter.horsePower).toEqual(100);
+    expect(newCharacter.torque).toEqual(800);
+    expect(newCharacter.nos).toEqual(0);
+    expect(newCharacter.playerLvl).toEqual(1);
+  })
 });
