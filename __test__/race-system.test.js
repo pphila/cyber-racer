6e86cdff2f3c7f2518b80bc14ca7bbe5c9d5a2fa
pphila <efl2ice89@gmail.com>
2022-10-03 20:18:59 -0700

    add logic to pass second test for race-systems
    Co-authored-by: David Marchuk
    <davormarchuk@gmail.com>
    Co-authored by: Hans Ellis
    <bullhorn@gmail.com>

--- __test__/race-system.test.js
+++ __test__/race-system.test.js
@@ -16,9 +16,14 @@ describe("Race system", () => {
   });
 
   test('it should take playerOne input and npc input to determine winner', () => {
-    const p1Dice = 4;
-    const npcDice = 6;
-
-    expect(newRace.runRace('win')).toBe(true);
+    const charName = "Dave";
+    const charCar = "electric";
+    const stage = 1;
+    
+    const playerCharacter = new Character(charName,charCar);
+    const npcEnemy = new Character("Big Bubba","monster truck"); //need to add utility
+    const newRace = new RaceSystem(playerCharacter, npcEnemy, stage);
+    
+    expect(newRace.runRace()).toEqual(true);
   })
 })
